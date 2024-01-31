55b3ccdade7605480a1ba2f6b876700110387208
pberlia0202 <pberlia0202@gmail.com>
2024-02-01 02:04:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+import Grid from "./components/Grid";
+function App() {
+  return (
+    <div className="App">
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
